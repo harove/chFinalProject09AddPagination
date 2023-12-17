840d1d7ebe7279263d7eeed3a991638fe11502e7
Edgar Haro <harove@gmail.com>
2023-12-17 18:14:26 -0300

    add and delete products in carts

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -1,5 +1,6 @@
 // import { cartsManager as manager } from "../dao/cartsManager.js"
 import { cartsManager as manager } from "../dao/index.js"
+import util from 'node:util'
 
 //creating cart
 export async function postController(req, res) {
@@ -29,12 +30,20 @@ export async function getByIdController(req, res) {
 //add product to cart
 export async function addProductToCartController(req, res) {
     const cid = req.params.cid
-    const pid = req.params.pid   
+    const pid = req.params.pid
     try {
         // const pojo = await manager.addProductToCart({cid,pid})
         const cart = await manager.findById(cid)
-        cart.products.push(pid)
-        cart.save()
+        console.log({pid})
+        const pidIndex = cart.products.findIndex(product=>product.id === pid)
+        console.log({pidIndex})
+        if (pidIndex === -1){
+            cart.products.push({id:pid, quantity:1})
+            await cart.save()
+        }else{
+            cart.products[pidIndex] = {...cart.products[pidIndex], quantity: cart.products[pidIndex].quantity+1}
+        }
+        await cart.save()
         res.json(cart)
     } catch (error) {
         res.status(404).json({
@@ -47,13 +56,19 @@ export async function addProductToCartController(req, res) {
 //delete product from cart
 export async function deleteProductFromCartController(req, res) {
     const cid = req.params.cid
-    const pid = req.params.pid   
+    const pid = req.params.pid
     try {
         const cart = await manager.findById(cid)
-        console.log({cart})
-        const productIndex = cart.products.findIndex(product=>product._id===pid)
-        // cart.save()
-        // res.json(cart)
+        console.log(util.inspect(cart,false,4))
+        const productIndex = cart.products.findIndex(product=>product.id===pid)
+        console.log({productIndex})
+        if(productIndex !== -1){
+            cart.products.splice(productIndex, 1);
+        }else{
+            throw new Error('Producto no encontrado')
+        }        
+        await cart.save()
+        res.json(cart)
     } catch (error) {
         res.status(404).json({
             mensaje: error.message
