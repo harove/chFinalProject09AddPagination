745eddb69c922bd85d68ec735ff5196dbfb280c6
Edgar Haro <harove@gmail.com>
2023-12-20 03:26:07 -0300

    cart view

--- src/routers/web.Router.js
+++ src/routers/web.Router.js
@@ -2,13 +2,11 @@ import { Router } from 'express';
 // import { productsManager as manager } from '../dao/productsManager.js';
 import { productsManager as manager } from '../dao/index.js';
 import { getByIdController, getController } from '../controllers/web/products.controller.js';
+import { cartsWebRouter } from './carts.web.router.js';
 
 export const webRouter = Router();
 webRouter.get('/products', getController);
 webRouter.get('/products/:id', getByIdController);
-
-
-
 
 webRouter.get('/realtimeproducts', async (req, res) => {
     // const products = await manager.findAll();
@@ -22,3 +20,5 @@ webRouter.get('/realtimeproducts', async (req, res) => {
 webRouter.get('/chat', (req,res)=>{
     res.render('chat.handlebars', {titulo:'Chat'})
 })
+
+webRouter.use('/carts', cartsWebRouter)
